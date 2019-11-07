cb84a15651b8e171f21b4585ba0d834376b7a60e
Kenvix <kenvixzure@live.com>
2019-11-07 22:37:35 +0800

    Better StringOutputStream

--- src/main/java/com/kenvix/utils/lang/StringOutputStream.java
+++ src/main/java/com/kenvix/utils/lang/StringOutputStream.java
@@ -8,13 +8,17 @@ package com.kenvix.utils.lang;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 /**
- * String Output Stream.
+ * String Output Stream implement.
  * It has a buffer, and accepts a callback which will be called when a byte[] inputted or separator appears or flush() is called.
  * Then flush the buffer after callback done.
+ *
+ * Thread Unsafe.
  */
 public class StringOutputStream extends OutputStream implements AutoCloseable {
     private Consumer<String> callback;
@@ -41,6 +45,9 @@ public class StringOutputStream extends OutputStream implements AutoCloseable {
     }
 
 
+    /**
+     * Invoke callback and flush data
+     */
     @Override
     public void flush() {
         if (count > 0) {
@@ -54,6 +61,44 @@ public class StringOutputStream extends OutputStream implements AutoCloseable {
     @Override
     public void close() {
         flush();
+    }
+
+    /**
+     * Increases the capacity if necessary to ensure that it can hold
+     * at least the number of elements specified by the minimum
+     * capacity argument.
+     *
+     * @param minCapacity the desired minimum capacity
+     * @throws OutOfMemoryError if {@code minCapacity < 0}.  This is
+     * interpreted as a request for the unsatisfiably large capacity
+     * {@code (long) Integer.MAX_VALUE + (minCapacity - Integer.MAX_VALUE)}.
+     */
+    private void ensureCapacity(int minCapacity) {
+        // overflow-conscious code
+        if (minCapacity - bytes.length > 0)
+            grow(minCapacity);
+    }
+
+    /**
+     * Writes the complete contents of this string output stream to
+     * the specified output stream argument, as if by calling the output
+     * stream's write method using <code>out.write(buf, 0, count)</code>.
+     *
+     * @param      out   the output stream to which to write the data.
+     * @exception IOException  if an I/O error occurs.
+     */
+    public void writeTo(OutputStream out) throws IOException {
+        out.write(bytes, 0, count);
+    }
+
+    /**
+     * Resets the <code>count</code> field of this string output
+     * stream to zero, so that all currently accumulated output in the
+     * output stream is discarded. The output stream can be used again,
+     * reusing the already allocated buffer space.
+     */
+    public void reset() {
+        count = 0;
     }
 
     /**
@@ -101,6 +146,32 @@ public class StringOutputStream extends OutputStream implements AutoCloseable {
      */
     public int getCount() {
         return count;
+    }
+
+    /**
+     * Increases the capacity to ensure that it can hold at least the
+     * number of elements specified by the minimum capacity argument.
+     *
+     * @param minCapacity the desired minimum capacity
+     */
+    private void grow(int minCapacity) {
+        // overflow-conscious code
+        int oldCapacity = bytes.length;
+        int newCapacity = oldCapacity << 1;
+        if (newCapacity - minCapacity < 0)
+            newCapacity = minCapacity;
+        if (newCapacity - MAX_ARRAY_SIZE > 0)
+            newCapacity = hugeCapacity(minCapacity);
+        bytes = Arrays.copyOf(bytes, newCapacity);
+    }
+
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+                Integer.MAX_VALUE :
+                MAX_ARRAY_SIZE;
     }
 
     /**
