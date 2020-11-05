801c35bdce3286e0015edc3a57fc6e808740a51c
Kenvix Zure <kenvixzure@live.com>
2020-11-05 11:50:12 +0800

    Add DirectBufferPool

--- src/main/java/com/kenvix/utils/lang/DirectBufferPool.kt
+++ src/main/java/com/kenvix/utils/lang/DirectBufferPool.kt
@@ -1,44 +1,38 @@
 package com.kenvix.utils.lang
 
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import java.nio.ByteBuffer
 
-object DirectBufferPool {
+class DirectBufferPool private constructor(
+    size: Int,
+    maxNum: Int,
+    private val buffers: Channel<ByteBuffer>
+) {
     /**
      * Buffer size. Default 4 MiB
      */
-    private var bufferSize: Int = 0
+    private val bufferSize: Int = size
 
     /**
      * Buffer Max Num. Default 2
      */
-    private var bufferNum: Int = 0
-
-    private lateinit var buffers: Channel<ByteBuffer>
-
-    private val setupMutex = Mutex()
-
-    suspend fun setupBuffer(size: Int = 1 shl 22, maxNum: Int = 2) {
-        setupMutex.withLock {
-            if (this::buffers.isInitialized) {
-                throw IllegalStateException("DirectBufferPool already initialized")
-            }
-
-            bufferNum = maxNum
-            bufferSize = size
-            buffers = Channel(maxNum)
-
-            for (i in 0 until maxNum) {
-                buffers.send(ByteBuffer.allocateDirect(size))
-            }
-        }
-    }
+    private val bufferNum: Int = maxNum
 
     suspend fun useBuffer(then: (suspend (ByteBuffer) -> Unit)) {
         val buffer = buffers.receive()
         then(buffer)
         buffers.send(buffer)
+    }
+
+    companion object {
+        @JvmStatic
+        suspend fun of(size: Int = 1 shl 22, maxNum: Int = 2): DirectBufferPool {
+            val channel = Channel<ByteBuffer>(maxNum)
+            for (i in 0 until maxNum) {
+                channel.send(ByteBuffer.allocateDirect(size))
+            }
+
+            return DirectBufferPool(size, maxNum, channel)
+        }
     }
 }
